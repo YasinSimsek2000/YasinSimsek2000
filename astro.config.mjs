c77be24f108d517e286d0af9e8a570163ea2b201
YasinSimsek2000 <simsekyasin2900@gmail.com>
2026-05-18 22:52:35 +0300

    deploy.yml file

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,7 @@ import mdx from '@astrojs/mdx';
 export default defineConfig({
   // Replace 'your-username' and 'your-repo' with your GitHub username and repo name
   // If using a custom domain, set site to 'https://yourdomain.com' and remove base
-  site: 'https://YasinSimsek2000.github.io',
+  site: 'https://YasinSimsek.github.io',
   base: '/YasinSimsek2000/',
   output: 'static',
   integrations: [mdx()],
